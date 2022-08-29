6f550d7743240aee8cbc7f8778f1990189c200e4
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-29 10:14:06 -0300

    att

--- angularjs/src/app/components/first-component/first-component.component.ts
+++ angularjs/src/app/components/first-component/first-component.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
-
+ name: string  = "Matheus"
+ age : number = 20
   constructor() { }
 
   ngOnInit(): void {
