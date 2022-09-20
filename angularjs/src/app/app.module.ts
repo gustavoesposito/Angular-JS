29ddf1c2936b3b4d4b88188726213ca6c86b3057
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-20 11:51:50 -0300

    att

--- angularjs/src/app/app.module.ts
+++ angularjs/src/app/app.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './components/first-component/first-component.component';
-import { ParentDataComponent } from './components/parent-data/parent-data.component';
+ 
 import { FirstCompoentComponent } from './components/first-compoent/first-compoent.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FirstComponentComponent,
-    ParentDataComponent,
-    FirstCompoentComponent
+    
   ],
   imports: [
     BrowserModule
